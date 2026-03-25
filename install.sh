45b0cc368928e62f5c9b6a38e20b0f704d2acaa0
Ghs Julian <98700347+ghsjulian@users.noreply.github.com>
2026-03-25 14:09:47 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+clear
+
+echo ""
+
+echo Welcome to linux
+
+echo ""
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install git
+
+sudo apt install curl
+
+sudo apt install wget
+
+sudo apt install python3
+
+sudo apt install php
+
+sudo apt install nodejs-lts
+
+
+
+
+
+
